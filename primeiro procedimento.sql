d9ebb97d66dc48a215f31fac97f34430f3377d2c
BRENO F SOUZA <148398192+BrenoSouza2023@users.noreply.github.com>
2024-05-14 22:20:55 -0300

    Update primeiro procedimento.sql

--- primeiro procedimento.sql
+++ primeiro procedimento.sql
@@ -1,103 +1,88 @@
-CREATE TABLE usuarios (
-  idusuarios INTEGER   NOT NULL ,
-  usuarios VARCHAR(20)    ,
-  senha VARCHAR(6)    ,
-  tipousuarios VARCHAR(20)      ,
-PRIMARY KEY(idusuarios));
-
-
-
-
 CREATE TABLE produtos (
   idprodutos INTEGER   NOT NULL ,
   pdt_nome VARCHAR(20)    ,
   pdt_quantidade INTEGER    ,
-  pdt_preco DECIMAL    ,
-  pdt_movimento VARCHAR(10)    ,
-  pdt_tipo VARCHAR(2)      ,
+  pdt_preco DECIMAL      ,
 PRIMARY KEY(idprodutos));
 
 
 
 
-CREATE TABLE p_juridica (
-  idp_juridica INTEGER   NOT NULL ,
+CREATE TABLE pessoa (
+  idpessoa INTEGER   NOT NULL ,
+  nome VARCHAR(100)    ,
+  logradouro VARCHAR(100)    ,
+  cidade VARCHAR(50)    ,
+  estado VARCHAR(50)    ,
+  telefone VARCHAR(20)    ,
+  email VARCHAR(60)      ,
+PRIMARY KEY(idpessoa));
+
+
+
+
+CREATE TABLE usuarios (
+  idusuarios INTEGER   NOT NULL ,
+  usuarios VARCHAR(20)    ,
+  senha VARCHAR(6)      ,
+PRIMARY KEY(idusuarios));
+
+
+
+
+CREATE TABLE pessoa_juridica (
+  pessoa_idpessoa INTEGER   NOT NULL ,
+  cnpj VARCHAR(20)      ,
+PRIMARY KEY(pessoa_idpessoa)  ,
+  FOREIGN KEY(pessoa_idpessoa)
+    REFERENCES pessoa(idpessoa));
+
+
+CREATE INDEX pessoa_juridica_FKIndex1 ON pessoa_juridica (pessoa_idpessoa);
+
+
+CREATE INDEX IFK_Rel_08 ON pessoa_juridica (pessoa_idpessoa);
+
+
+CREATE TABLE pessoa_fisica (
+  pessoa_idpessoa INTEGER   NOT NULL ,
+  cpf VARCHAR(20)      ,
+PRIMARY KEY(pessoa_idpessoa)  ,
+  FOREIGN KEY(pessoa_idpessoa)
+    REFERENCES pessoa(idpessoa));
+
+
+CREATE INDEX pessoa_fisica_FKIndex1 ON pessoa_fisica (pessoa_idpessoa);
+
+
+CREATE INDEX IFK_Rel_09 ON pessoa_fisica (pessoa_idpessoa);
+
+
+CREATE TABLE movimento (
+  idmovimento INTEGER   NOT NULL ,
   usuarios_idusuarios INTEGER   NOT NULL ,
-  pj_nome VARCHAR(45)    ,
-  pj_cnpj INTEGER    ,
-  pj_logradouro VARCHAR(20)    ,
-  pj_numero INTEGER    ,
-  pj_contato INTEGER      ,
-PRIMARY KEY(idp_juridica)  ,
-  FOREIGN KEY(usuarios_idusuarios)
-    REFERENCES usuarios(idusuarios));
-
-
-CREATE INDEX p_juridica_FKIndex1 ON p_juridica (usuarios_idusuarios);
-
-
-CREATE INDEX IFK_Rel_07 ON p_juridica (usuarios_idusuarios);
-
-
-CREATE TABLE p_fisica (
-  idp_fisica INTEGER   NOT NULL ,
-  usuarios_idusuarios INTEGER   NOT NULL ,
-  pf_nome VARCHAR(45)    ,
-  pf_cpf INTEGER    ,
-  pf_idade INTEGER    ,
-  pf_logradouro VARCHAR(20)    ,
-  pf_numero INTEGER    ,
-  contato INTEGER      ,
-PRIMARY KEY(idp_fisica, usuarios_idusuarios)  ,
-  FOREIGN KEY(usuarios_idusuarios)
-    REFERENCES usuarios(idusuarios));
-
-
-CREATE INDEX p_fisica_FKIndex1 ON p_fisica (usuarios_idusuarios);
-
-
-CREATE INDEX IFK_Rel_08 ON p_fisica (usuarios_idusuarios);
-
-
-CREATE TABLE p_juridica_has_produtos (
-  p_juridica_idp_juridica INTEGER   NOT NULL ,
-  produtos_idprodutos INTEGER   NOT NULL   ,
-PRIMARY KEY(p_juridica_idp_juridica, produtos_idprodutos)    ,
-  FOREIGN KEY(p_juridica_idp_juridica)
-    REFERENCES p_juridica(idp_juridica),
-  FOREIGN KEY(produtos_idprodutos)
-    REFERENCES produtos(idprodutos));
-
-
-CREATE INDEX p_juridica_has_produtos_FKIndex1 ON p_juridica_has_produtos (p_juridica_idp_juridica);
-CREATE INDEX p_juridica_has_produtos_FKIndex2 ON p_juridica_has_produtos (produtos_idprodutos);
-
-
-CREATE INDEX IFK_Rel_03 ON p_juridica_has_produtos (p_juridica_idp_juridica);
-CREATE INDEX IFK_Rel_04 ON p_juridica_has_produtos (produtos_idprodutos);
-
-
-CREATE TABLE p_fisica_has_produtos (
-  p_fisica_idp_fisica INTEGER   NOT NULL ,
+  pessoa_idpessoa INTEGER   NOT NULL ,
   produtos_idprodutos INTEGER   NOT NULL ,
-  p_fisica_usuarios_idusuarios INTEGER   NOT NULL   ,
-PRIMARY KEY(p_fisica_idp_fisica, produtos_idprodutos, p_fisica_usuarios_idusuarios)    ,
-  FOREIGN KEY(p_fisica_idp_fisica, p_fisica_usuarios_idusuarios)
-    REFERENCES p_fisica(idp_fisica, usuarios_idusuarios),
+  quantidade INTEGER    ,
+  tipo VARCHAR(20)    ,
+  valorUnitario DECIMAL      ,
+PRIMARY KEY(idmovimento)      ,
+  FOREIGN KEY(usuarios_idusuarios)
+    REFERENCES usuarios(idusuarios),
+  FOREIGN KEY(pessoa_idpessoa)
+    REFERENCES pessoa(idpessoa),
   FOREIGN KEY(produtos_idprodutos)
     REFERENCES produtos(idprodutos));
 
 
-CREATE INDEX p_fisica_has_produtos_FKIndex1 ON p_fisica_has_produtos (p_fisica_idp_fisica, p_fisica_usuarios_idusuarios);
-CREATE INDEX p_fisica_has_produtos_FKIndex2 ON p_fisica_has_produtos (produtos_idprodutos);
+CREATE INDEX movimento_FKIndex1 ON movimento (usuarios_idusuarios);
+CREATE INDEX movimento_FKIndex2 ON movimento (pessoa_idpessoa);
+CREATE INDEX movimento_FKIndex3 ON movimento (produtos_idprodutos);
 
 
-CREATE INDEX IFK_Rel_05 ON p_fisica_has_produtos (p_fisica_idp_fisica, p_fisica_usuarios_idusuarios);
-CREATE INDEX IFK_Rel_06 ON p_fisica_has_produtos (produtos_idprodutos);
+CREATE INDEX IFK_Rel_07 ON movimento (usuarios_idusuarios);
+CREATE INDEX IFK_Rel_08 ON movimento (pessoa_idpessoa);
+CREATE INDEX IFK_Rel_09 ON movimento (produtos_idprodutos);
 
 
-CREATE SEQUENCE PessoaIDSeq
-    START WITH 1
-    INCREMENT BY 1
-    NO CYCLE;
 
